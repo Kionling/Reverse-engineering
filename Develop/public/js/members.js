43429f0e6ec64f8d7f86350e0b268cf557ddf83e
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-29 21:03:13 -0700

    Members.js comments created

--- Develop/public/js/members.js
+++ Develop/public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
+    //by referencing the specific class of memeber name the program recognizes what kind of data will be received 
     $(".member-name").text(data.email);
   });
 });
